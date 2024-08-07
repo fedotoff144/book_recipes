7f87bcabe04a68ff8a43d648e8251aa164210198
fedotoff144 TecnoLaptop <fedotoff144@gmail.com>
2024-08-07 16:53:42 +0300

    work with templates, cant connect style.css to base.html

--- bookrecipes/bookrecipes/urls.py
+++ bookrecipes/bookrecipes/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from recipeapp.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('recipeapp/', include('recipeapp.urls')),
+    path('userapp/', include('userapp.urls')),
+    path('', index, name='index'),
+
 ]
