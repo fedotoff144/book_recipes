3e856a7cb2087a020900c1d3f6e35ad482ebffd8
fedotoff144 GigabyteLaptop <fedotoff144@gmail.com>
2024-08-08 23:37:06 +0300

    connect some templates to view

--- bookrecipes/userapp/urls.py
+++ bookrecipes/userapp/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from userapp.views import help_template, rules, advertisement
 
 urlpatterns = [
-
+    path('help/', help_template, name='help_template'),
+    path('rules/', rules, name='rules'),
+    path('advertisement/', advertisement, name='advertisement'),
 ]
--- bookrecipes/userapp/views.py
+++ bookrecipes/userapp/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
 
+
+def advertisement(request):
+    return render(request, 'userapp/advertisement.html')
+
+
+def help_template(request):
+    return render(request, 'userapp/help.html')
+
+
+def rules(request):
+    return render(request, 'userapp/rules.html')
 
